52410f2f5fe93b77f2879e56da670c68fdab546b
Bernardo Mendes <134334489+brnd09@users.noreply.github.com>
2024-04-28 20:18:43 -0300

    Update AppMain.java

--- AppMain.java
+++ AppMain.java
@@ -1,9 +1,40 @@
-package com.mycompany.atividade02;
+package org.example;
 
-public class AppMain {
-
+public class Main {
     public static void main(String[] args) {
-        
-        
+        // Entrada dos nomes do Oráculo e do Guerreiro
+        String nomeOraculo = InOut.leString("Digite o nome do Oráculo: ");
+        String nomeGuerreiro = InOut.leString("Digite o nome do Guerreiro: ");
+
+        // Criação do Oráculo Master e do Guerreiro
+        Oraculo oraculoMaster = new Oraculo(nomeOraculo);
+        Guerreiro guerreiro = new Guerreiro(nomeGuerreiro);
+
+        // Oráculo sorteia vidas para o Guerreiro
+        guerreiro.setQtdVidas(oraculoMaster.sortearVidas());
+
+        // Exibe o prólogo do jogo
+        InOut.MsgDeInformacao("Introdução", oraculoMaster.prologoIntroducao(guerreiro));
+
+        // Joga a primeira partida (level 1)
+        boolean resultadoPartida1 = oraculoMaster.loadLevel1(guerreiro);
+
+        // Estado do Guerreiro após a primeira partida
+        InOut.MsgDeInformacao("Estado do Guerreiro após a primeira partida",
+                "Nome: " + guerreiro.getNome_guerreiro() + "\nVidas: " + guerreiro.getQtdVidas());
+
+        // Joga a segunda partida (level 2)
+        boolean resultadoPartida2 = oraculoMaster.loadLevel2(guerreiro);
+
+        // Estado do Guerreiro após a segunda partida
+        InOut.MsgDeInformacao("Estado do Guerreiro após a segunda partida",
+                "Nome: " + guerreiro.getNome_guerreiro() + "\nVidas: " + guerreiro.getQtdVidas());
+
+        // Exibe o resultado do jogo (baseado nas duas partidas)
+        if (resultadoPartida1 && resultadoPartida2) {
+            InOut.MsgDeInformacao("Resultado Final", oraculoMaster.prologoVencedor());
+        } else {
+            InOut.MsgDeInformacao("Resultado Final", oraculoMaster.prologoPerdedor());
+        }
     }
 }
