c791e2643b00f8c9811d5b77d503c0168402463c
Bernardo Mendes <134334489+brnd09@users.noreply.github.com>
2024-04-28 20:18:59 -0300

    Update Guerreiro.java

--- Guerreiro.java
+++ Guerreiro.java
@@ -1,16 +1,39 @@
-package com.mycompany.atividade02;
+package org.example;
 
 public class Guerreiro {
+    private String nome_guerreiro;  // Nome do Guerreiro
+    private int qtdVidas;  // Quantidade de vidas
 
-    String nome, vidaExtra;
-    int qtdVidas;
-
-    public String vidaExtra(String pedido) {
-        return "Pedido de Misericórdia: " + pedido;
+    public Guerreiro(String nome) {
+        this.nome_guerreiro = nome;  // Define o nome do Guerreiro
+        this.qtdVidas = 0;  // Inicializa as vidas
     }
 
-    String getNome() {
-        return InOut.leString(nome);
+    public void definirNome(String nome) {
+        this.nome_guerreiro = nome;  // Permite redefinir o nome
     }
 
+    public void ganharVida() {
+        this.qtdVidas++;  // Guerreiro ganha uma vida
+    }
+
+    public void perderVida() {
+        this.qtdVidas--;  // Guerreiro perde uma vida
+    }
+
+    public String getNome_guerreiro() {
+        return nome_guerreiro;  // Retorna o nome do Guerreiro
+    }
+
+    public int getQtdVidas() {
+        return qtdVidas;  // Retorna a quantidade de vidas
+    }
+
+    public void setQtdVidas(int qtdVidas) {
+        this.qtdVidas = qtdVidas;  // Permite definir a quantidade de vidas
+    }
+
+    public String vidaExtra() {
+        return InOut.leString("Guerreiro " + nome_guerreiro + ", faça um pedido de misericórdia por uma vida extra: ");
+    }
 }
