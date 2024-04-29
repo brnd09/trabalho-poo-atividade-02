6b7df2a3fbcfd33d5f6d802a451158fcb30a0e17
Bernardo Mendes <134334489+brnd09@users.noreply.github.com>
2024-04-29 10:16:21 -0300

    Update Oraculo.java

--- Oraculo.java
+++ Oraculo.java
@@ -1,84 +1,34 @@
-import java.util.Random;
+public class Guerreiro {
+    private String nome_guerreiro;
+    private int qtdVidas;
 
-public class Oraculo {
-    private String nome_oraculo;  // Nome do Oráculo
-    private final Random random = new Random();  // Para sorteios
-
-    public Oraculo(String nome) {
-        this.nome_oraculo = nome;  // Define o nome do Oráculo ao instanciar
+    public Guerreiro(String nome) {
+        this.nome_guerreiro = nome;
+        this.qtdVidas = 0;
     }
 
-    public void definirNome(String nome) {
-        this.nome_oraculo = nome;  // Permite redefinir o nome do Oráculo
+    public void ganharVida() {
+        this.qtdVidas++;
     }
 
-    public int sortearVidas() {
-        return 9 + random.nextInt(4);  // Retorna um número entre 9 e 12
+    public void perderVida() {
+        this.qtdVidas--;
     }
 
-    public String prologoIntroducao(Guerreiro guerreiro) {
-        return "Oráculo " + nome_oraculo + ":\n" +
-                "\nGuerreiro " + guerreiro.getNome_guerreiro() + ", você terá " +
-                guerreiro.getQtdVidas() + " vidas para enfrentar os desafios.\n" +
-                "Prepare-se para a aventura!";
+    public String getNome_guerreiro() {
+        return nome_guerreiro;
     }
 
-    public boolean loadLevel1(Guerreiro guerreiro) {
-        int numeroSorteado = 1 + random.nextInt(100);  // Sorteia um número entre 1 e 100
-        int palpiteGuerreiro;
-        boolean acertou = false;
-
-        do {
-            palpiteGuerreiro = InOut.leInt("Guerreiro " + guerreiro.getNome_guerreiro() + ", faça um palpite entre 1 e 100: ");
-
-            if (palpiteGuerreiro < numeroSorteado) {
-                InOut.MsgDeAviso("Dica", "O segredo é MAIOR que o seu palpite.");
-            } else if (palpiteGuerreiro > numeroSorteado) {
-                InOut.MsgDeAviso("Dica", "O segredo é MENOR que o seu palpite.");
-            } else {
-                InOut.MsgDeInformacao("Parabéns", "Parabéns, Guerreiro " + guerreiro.getNome_guerreiro() + "! Você acertou!");
-                acertou = true;
-            }
-
-            guerreiro.perderVida();  // Cada erro perde uma vida
-            InOut.MsgDeAviso("Vidas", "Você perdeu uma vida! Restam " + guerreiro.getQtdVidas() + " vidas.");
-
-        } while (!acertou && guerreiro.getQtdVidas() > 0);  // Continua até acertar ou acabar as vidas
-
-        return acertou;  // Retorna se o Guerreiro ganhou ou perdeu
+    public int getQtdVidas() {
+        return qtdVidas;
     }
 
-    public boolean loadLevel2(Guerreiro guerreiro) {
-        String escolha = InOut.leString("Guerreiro " + guerreiro.getNome_guerreiro() + ", escolha PAR ou ÍMPAR: ").toUpperCase();
-
-        int numeroGuerreiro = random.nextInt(6);  // Número entre 0 e 5
-        int numeroOraculo = random.nextInt(6);  // Número entre 0 e 5
-        int soma = numeroGuerreiro + numeroOraculo;  // Soma dos números
-
-        boolean ehPar = (soma % 2 == 0);  // Verifica se é par ou ímpar
-
-        boolean guerreiroGanhou = (escolha.equals("PAR") && ehPar) || (escolha.equals("ÍMPAR") && !ehPar);  // Verifica se o Guerreiro ganhou
-
-        if (guerreiroGanhou) {
-            InOut.MsgDeInformacao("Resultado", "Você ganhou! A soma foi " + soma + ", que é " + (ehPar ? "PAR" : "ÍMPAR"));
-        } else {
-            InOut.MsgDeInformacao("Resultado", "Você perdeu! A soma foi " + soma + ", que é " + (ehPar ? "PAR" : "ÍMPAR"));
-        }
-
-        return guerreiroGanhou;  // Retorna se o Guerreiro ganhou ou perdeu
+    public void setQtdVidas(int qtdVidas) {
+        this.qtdVidas = qtdVidas;
     }
 
-    public String prologoVencedor() {
-        return "Parabéns, Guerreiro! Você venceu o jogo!";
-    }
-
-    public String prologoPerdedor() {
-        return "Infelizmente, Guerreiro, você perdeu o jogo. Tente novamente!";
-    }
-
-    public boolean decidirVidaExtra(String pedido) {
-        String[] palavras = pedido.split("\\s+");  // Dividido por espaços
-
-        return palavras.length > 5;  // Concede vida extra se o pedido tiver mais que cinco palavras
+    // Defina o método para pedir uma vida extra
+    public String pedirVidaExtra() {
+        return InOut.leString("Guerreiro " + nome_guerreiro + ", faça um pedido por uma vida extra: ");
     }
 }
